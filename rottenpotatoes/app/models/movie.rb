0152c41bec107be9d16c1d2fec47c22444f3b4cb
melonskin <tsjtux@gmail.com>
2017-02-27 20:52:27 -0600

    2 rspec tests finished

--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -2,4 +2,9 @@ class Movie < ActiveRecord::Base
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
   end
+
+  def  self.same_director(director)
+    Movie.where(:director => director) if (not director.nil?) and (not director.empty?)
+  end
+
 end
